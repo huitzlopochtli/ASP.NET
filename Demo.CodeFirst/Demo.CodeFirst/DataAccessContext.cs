9ba89462f1b4b2790f1041a0cf2daa26ab9d462d
Peeyal Khondokar <peeyalk@hotmail.com>
2017-11-06 03:49:44 +0600

    Initialize the model class into Database

---
+++ Demo.CodeFirst/Demo.CodeFirst/DataAccessContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.CodeFirst.Model;
+
+namespace Demo.CodeFirst
+{
+    class DataAccessContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public DataAccessContext()
+            :base("name=DemoEntityFrameworkCodeFirst")
+        {
+            
+        }
+    }
+}
