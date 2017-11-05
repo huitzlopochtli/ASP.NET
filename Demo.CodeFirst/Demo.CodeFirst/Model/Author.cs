9a728521d1aeb16a6c346df387fb9767ea7ee07c
Peeyal Khondokar <peeyalk@hotmail.com>
2017-11-06 03:29:47 +0600

    Created all the model classes

---
+++ Demo.CodeFirst/Demo.CodeFirst/Model/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.CodeFirst.Model
+{
+    class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+    }
+}
--- Demo.CodeFirst/Demo.CodeFirst/Program.cs
+++ Demo.CodeFirst/Demo.CodeFirst/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Demo.CodeFirst
 {
+    [NotMapped]
     class Program
     {
         static void Main(string[] args)
@@ -14,3 +16,4 @@ namespace Demo.CodeFirst
         }
     }
 }
+  
