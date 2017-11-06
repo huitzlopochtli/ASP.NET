f9e35607eafc002d06c07deaf6dd717a27337068
Peeyal Khondokar <peeyalk@hotmail.com>
2017-11-07 03:21:45 +0600

    Entenstion method in program.cs

--- Demo.CodeFirst/Demo.CodeFirst/Program.cs
+++ Demo.CodeFirst/Demo.CodeFirst/Program.cs
@@ -16,17 +16,26 @@ namespace Demo.CodeFirst
             var context = new DataAccessContext();
 
             //LinQ Syntex
-            var query =
-                from c in context.Courses
-                where c.Name.Contains("C#")
-                orderby c.Name
-                select c;
+            //var query =
+            //    from c in context.Courses
+            //    where c.Name.Contains("C#")
+            //    orderby c.Name
+            //    select c;
+
+            //Entension methods
+            var query = context.Courses
+                .Where(c => c.Name.Contains("C#"))
+                .OrderBy(c => c.Name);
 
             foreach (var course in query)
             {
                 Console.WriteLine(course.Name);
             }
+
             
+
+            
+
         }
     }
 }
