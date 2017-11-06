f6c2137038d2d80501267d4f72871cf801065951
Peeyal Khondokar <peeyalk@hotmail.com>
2017-11-07 03:18:43 +0600

    LINQ syntex in program.cs

--- Demo.CodeFirst/Demo.CodeFirst/Program.cs
+++ Demo.CodeFirst/Demo.CodeFirst/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,20 @@ namespace Demo.CodeFirst
     {
         static void Main(string[] args)
         {
+            var context = new DataAccessContext();
 
+            //LinQ Syntex
+            var query =
+                from c in context.Courses
+                where c.Name.Contains("C#")
+                orderby c.Name
+                select c;
+
+            foreach (var course in query)
+            {
+                Console.WriteLine(course.Name);
+            }
+            
         }
     }
 }
