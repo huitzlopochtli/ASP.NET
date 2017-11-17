bba218082ff91276c61e09b88f4cec594673b423
Peeyal Khondokar <peeyalk@hotmail.com>
2017-11-18 04:21:09 +0600

    added IAuthorRepository Interface and Implemented AuthorRepository Class

---
+++ Demo.CodeFirst/Demo.Repository/Classes/AuthorRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.CodeFirst;
+using Demo.CodeFirst.Model;
+using Demo.Repository.Interfaces;
+
+namespace Demo.Repository.Classes
+{
+    public class AuthorRepository : Repository<Author>, IAuthorRepository
+    {
+        public AuthorRepository(DbContext context) : base(context)
+        {
+        }
+
+        public DataAccessContext DataAccessContext {
+            get { return Context as DataAccessContext; } }
+
+        public Author GetAuthorWithCourses(int id)
+        {
+            return DataAccessContext.Authors.Include(a => a.Courses).SingleOrDefault(a => a.Id == id);
+        }
+    }
+}
---
+++ Demo.CodeFirst/Demo.Repository/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,9 @@
+﻿using Demo.CodeFirst.Model;
+
+namespace Demo.Repository.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        Author GetAuthorWithCourses(int id);
+    }
+}
