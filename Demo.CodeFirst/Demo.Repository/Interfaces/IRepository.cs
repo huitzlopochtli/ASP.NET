a8d13e1aad9835835c6f41ebce92388761b3290f
Peeyal Khondokar <peeyalk@hotmail.com>
2017-11-18 03:34:48 +0600

    Added Folders for Classes and Interfaces in Repository

--- Demo.CodeFirst/Demo.Repository/Interfaces/IRepository.cs
+++ Demo.CodeFirst/Demo.Repository/Interfaces/IRepository.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demo.Repository
+namespace Demo.Repository.Interfaces
 {
     public interface IRepository<TEntity> where TEntity : class
     {
